626204f1f941be11d039548989efd90f4f6bf96e
Miguel Covarrubias <mcovarr@broadinstitute.org>
2018-11-01 19:15:02 -0400

    oops

--- backend/src/test/scala/cromwell/backend/standard/callcaching/BlacklistCacheSpec.scala
+++ backend/src/test/scala/cromwell/backend/standard/callcaching/BlacklistCacheSpec.scala
@@ -9,7 +9,7 @@ import scala.language.postfixOps
 class BlacklistCacheSpec extends FlatSpec with BeforeAndAfterAll with Matchers {
   "The blacklist cache" should "default, blacklist and expire" in {
     val bucket = "foo"
-    val cacheConfig = CacheConfig(concurrency = 1, size = Integer.MAX_VALUE, ttl = 3 days)
+    val cacheConfig = CacheConfig(concurrency = 1, size = Integer.MAX_VALUE, ttl = 1 second)
     val cache = BlacklistCache(cacheConfig)
     cache.isBlacklisted(bucket) shouldBe false
     cache.blacklist(bucket)
